546c9eaa4a9b0ff55f9ab72f434c9b516889ceb3
Jameem <jameemkurikkal.mp@gmail.com>
2020-07-27 08:36:15 +0530

    boiler plate

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react"
+import "./App.css"
+import Post from "./Post"
+
+function App() {
+  return (
+    <div className="app">
+      <div className="app__header">
+        <img className="app__headerImage" src="logo.png" alt="" />
+      </div>
+    </div>
+  )
+}
+
+export default App
