906af7788d4c1fdd7e1ebc6339c8c55f57889d2a
Jameem <jameemkurikkal.mp@gmail.com>
2020-07-27 22:09:27 +0530

    signin modal

---
+++ src/SignInModal.js
@@ -0,0 +1,12 @@
+import React from "react"
+import { makeStyles } from "@material-ui/core/styles"
+import Modal from "@material-ui/core/Modal"
+
+import { db, auth } from "./firebase"
+import { Button, Input } from "@material-ui/core"
+
+function SignInModal() {
+  return <div></div>
+}
+
+export default SignInModal
